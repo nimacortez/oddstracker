8f6fa5f165de0f504ff3b78903429ebe2e391736
nimacortez <cortez.nima@gmail.com>
2024-08-06 22:28:53 -0400

    added api

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,26 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/events', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.the-odds-api.com/v4/sports', {
+      params: {
+        apiKey: '8c29f602d6e6e7e72c40df4086672e3e'
+      }
+    });
+    console.log('API response:', response.data);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching odds:', {
+      message: error.message,
+      code: error.code,
+      config: error.config,
+      response: error.response ? error.response.data : 'No response data'
+    });
+    res.status(500).send('Error fetching odds: ' + error.message);
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Server running on port ${5000}`);
+  console.log(`Server running on port ${port}`);
 });
